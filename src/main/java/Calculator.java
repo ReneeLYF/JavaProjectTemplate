b21ac43bd6df700fb51411acd4e6a8f7b9493918
ReneeLYF <109120956+ReneeLYF@users.noreply.github.com>
2023-07-03 10:30:52 +0800

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,11 +8,11 @@ public class Calculator implements ICalculator {
         screen.showResult(result);
         return result;
     }
-    public int delete(int a, int b) {
-        int result = a + b;
-        screen.showResult(result);
-        return result;
-    }
+    // public int delete(int a, int b) {
+    //     int result = a + b;
+    //     screen.showResult(result);
+    //     return result;
+    // }
 
     public static class Screen {
         private Brightness brightness;
