1689ea2a6e8f69e3f1deb761d2a72abb93cb6938
ReneeLYF <109120956+ReneeLYF@users.noreply.github.com>
2023-07-04 11:36:22 +0800

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,17 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    @Test
-    void testAdd() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(1, 2);
-        assertEquals(3, result);
-    }
+    // @Test
+    // void testAdd() {
+    //     Calculator calculator = new Calculator();
+    //     int result = calculator.add(1, 2);
+    //     assertEquals(3, result);
+    // }
 
-    @Test
-    void testMinus() {
-        Calculator calculator = new Calculator();
-        int result = calculator.minus(2, 1);
-        assertEquals(1, result);
-    }
+    // @Test
+    // void testMinus() {
+    //     Calculator calculator = new Calculator();
+    //     int result = calculator.minus(2, 1);
+    //     assertEquals(1, result);
+    // }
 }
