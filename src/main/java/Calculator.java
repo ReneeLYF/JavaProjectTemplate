42588dad76c70b8d9fb8fc642300b7b098c7b59d
ReneeLYF <109120956+ReneeLYF@users.noreply.github.com>
2023-08-17 13:26:43 +0800

    Update Calculator.java

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -23,7 +23,17 @@ public class Calculator implements ICalculator {
         public void showResult2(int result) {
             System.out.println("The result2 is: " + result);
         }
+        public void showResult3(int result) {
+            System.out.println("The result2 is: " + result);
+        }
 
+        public void showResult4(int result) {
+            System.out.println("The result2 is: " + result);
+        }
+
+        public void showResult5(int result) {
+            System.out.println("The result2 is: " + result);
+        }
 
         public enum Brightness {
             HIGH, MIDDLE, LOW
